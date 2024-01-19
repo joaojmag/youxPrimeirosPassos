0159eb936108edff1bf7366a90e3acc0af1415e2
João Marcelo <joaojmag@gmail.com>
2024-01-19 11:29:21 -0300

    atualizou exc 07

--- ExerVetores/07/index.js
+++ ExerVetores/07/index.js
@@ -1,12 +1,14 @@
-const vetBase = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let vetBase = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 const k = 5;
 const elementosDeK = [1, 2, 3, 4, 5];
 const posicaoElemento = 2;
 let novoElemento = 9;
 
-for (let i = 0; i < k; i++) {
-    vetBase[i] = elementosDeK[i];
-}
+// for (let i = 0; i < k; i++) {
+//     vetBase[i] = elementosDeK[i];
+// }
+
+vetBase = vetBase.map((valo, index) => valo = (typeof elementosDeK[index] === "undefined") ? 0 : elementosDeK[index])
 
 for (let j = posicaoElemento; j < vetBase.length; j++) {
     let salvaValor = vetBase[j]
