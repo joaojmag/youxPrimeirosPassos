d747791f5d7d6d2f48fe7c2a222240b6f62b3977
João Marcelo <joaojmag@gmail.com>
2024-01-22 10:53:14 -0300

    add exec 11

--- ExerVetores/11/index.js
+++ ExerVetores/11/index.js
@@ -1,1 +1,21 @@
 const n = 6;
+const caracteres = 'a b c d e f';
+const valorEntrada = 'c f';
+let nova = '';
+let boleano = false;
+
+for (let i in caracteres) {
+    if (caracteres[i] === valorEntrada[2]) {
+        boleano = false;
+    }
+    if (caracteres[i] === valorEntrada[0]) {
+        boleano = true;
+    }
+    if (boleano) {
+        nova += caracteres[i] 
+    }
+}
+
+console.log(caracteres.length)
+console.log(nova)
+// fiz mas não está 100% 
