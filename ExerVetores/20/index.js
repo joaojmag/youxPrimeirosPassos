3eed82602674300ba6d735b3782a60a22ad95761
João Marcelo <joaojmag@gmail.com>
2024-01-23 17:11:02 -0300

    add exec 20 não esta completo

---
+++ ExerVetores/20/index.js
@@ -0,0 +1,25 @@
+let num = 7;
+
+let valor = 0;
+do {
+    let numM = multNum(num);
+    let numS = numM.toString();
+    console.log(numS);
+
+
+
+} while (valor !== num && valor !== 1);
+
+function multNum(numero){
+    return numero * numero
+}
+
+
+let stringg = num.toString()
+
+console.log(typeof stringg)
+
+
+let numero = Number(stringg)
+
+console.log(typeof numero)
