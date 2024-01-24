c742b8255b7684da6e470dae67fb9dcf530a6abf
João Marcelo <joaojmag@gmail.com>
2024-01-24 10:04:56 -0300

    tentativa de fazer o 20 não deu certo ainda

--- ExerVetores/20/index.js
+++ ExerVetores/20/index.js
@@ -1,17 +1,31 @@
 let num = 7;
 
+let vetornumeros = [];
 let valor = 0;
 do {
     let numM = multNum(num);
     let numS = numM.toString();
+    let tamanhoS = numS.length;
+    for (let i = 0; i < tamanhoS; i++) {
+        vetornumeros[i] = stringNumero(numS[i]);
+    }
+    valor = stringNumero(numS);
+    for (let j = 0; j < vetornumeros.length; j++) {
+        multNum(vetornumeros[j])
+    }
     console.log(numS);
 
 
 
 } while (valor !== num && valor !== 1);
 
-function multNum(numero){
+
+function multNum(numero) {
     return numero * numero
+}
+
+function stringNumero(valor) {
+    return Number(valor)
 }
 
 
@@ -23,3 +37,6 @@ console.log(typeof stringg)
 let numero = Number(stringg)
 
 console.log(typeof numero)
+
+
+console.log(vetornumeros);
