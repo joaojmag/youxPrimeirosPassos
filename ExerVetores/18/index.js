80dfd09dcf8f97c2343a6e70af3af216eead9c08
joaojmag <joaojmag@gmail.com>
2024-02-05 23:53:02 -0300

    arquivos rem

--- ExerVetores/18/index.js
+++ ExerVetores/18/index.js
@@ -1,38 +1,14 @@
-// const valores = [2, 4, 1, 10, 4, 2, 1];
+const valores = [2, 4, 1, 10, 4, 2, 1];
 
-// const moda = valores.reduce()
+let vetContador = [];
+let contador = 0;
 
-
-// let novoVet = [];
-// let numCont = [];
-
-// for (let i in valores){
-//     let contador = 0;
-//     for(let j in valores){
-//         if (valores[i] === valores[j]){
-//             contador += 1;
-//         }
-//     }
-//     numCont.push(contador);
-// }
-
-// console.log(numCont);
-
-
-
-
-const nomes = [2, 4, 1, 10, 4, 2, 1];
-
-
-//let nomes = ["Alice", "Bob", "Tiff", "Bruce", "Alice"];
-
-let quantidadeNomes = nomes.reduce(function (todosNomes, nome) {
-  if (nome in todosNomes) {
-    todosNomes[nome]++;
-  } else {
-    todosNomes[nome] = 1;
+valores.forEach((e, j) => {
+  if (valores.includes(e)){
+    contador++;
+    vetContador[j] = contador;   
   }
-  return todosNomes;
-}, {});
+});
 
-console.log(quantidadeNomes)
+console.log(vetContador)
+
