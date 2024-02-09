c3757503fc0d23b76897af8a70ab3307b3dc0871
João Marcelo <joaojmag@gmail.com>
2024-02-09 08:53:36 -0300

    add 62

--- JavaScript/aula62_HerancaPOO.js
+++ JavaScript/aula62_HerancaPOO.js
@@ -11,10 +11,49 @@ class Carro { //Classe PAI
     setCor = (cor) => this.cor = cor
 }
 
-const c1 = new Carro('Nomrmal', 4)
+class Militar extends Carro {// Classe Filho
+    constructor(nome, portas, blindagem, municao) {
+        super(nome, portas)
+        this.blindagem = blindagem
+        this.municao = municao
+        this.setCor('Verde')
+    }
 
-console.log('c1.nome :>> ', c1['nome']);
-// console.log('portas :>> ', c1.portas);
-// console.log('c1.lidado :>> ', c1.ligado);
-// console.log('c1.vel :>> ', c1.vel);
-// console.log('c1.cor :>> ', c1.cor);
+    atirar = function () {
+        if (this.municao > 0) {
+            this.municao--
+        }
+    }
+}
+
+class Utilitario extends Carro {
+    constructor(nome, portas, lugares) {
+        super(nome, portas)
+        this.lugares = lugares
+    }
+}
+
+const c1 = new Carro('Nomrmal', 4)
+c1.isLigar()
+c1.setCor('Preto')
+
+console.log(`Normal: ${c1.nome}`);
+console.log(`Portas: ${c1.portas}`);
+console.log(`Lidado: ${c1.ligado ? 'Sim' : 'Não'}`);
+console.log(`Velocidade ${c1.vel}`);
+console.log(`Cor ${c1.cor}`);
+console.log('------------------------------------');
+
+const c2 = new Militar('Lutador', 1, 100, 15)
+c2.setCor('Azul')
+c2.atirar()
+c2.atirar()
+console.log(`Normal: ${c2.nome}`);
+console.log(`Portas: ${c2.portas}`);
+console.log(`Lidado: ${c2.ligado ? 'Sim' : 'Não'}`);
+console.log(`Velocidade ${c2.vel}`);
+console.log(`Blingagem ${c2.blindagem}`);
+console.log(`Munição ${c2.municao}`);
+
+console.log(`Cor ${c2.cor}`);
+console.log('------------------------------------');
