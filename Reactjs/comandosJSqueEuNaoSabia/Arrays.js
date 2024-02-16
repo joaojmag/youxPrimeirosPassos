153a2db2d532fd7d01ff36ddaeef3a89d978299b
João Marcelo <joaojmag@gmail.com>
2024-02-16 09:19:34 -0300

    começando no React

---
+++ Reactjs/comandosJSqueEuNaoSabia/Arrays.js
@@ -0,0 +1,10 @@
+const arr = ['joao', 'pedro', 'vitor'];
+
+console.log(arr.indexOf('pedro'));
+
+console.log(arr.join(' / '));
+
+let a = 'joao'
+
+if (arr.indexOf(a) == -1)
+    console.log('Não tem');
