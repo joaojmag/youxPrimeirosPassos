d8627c4b9763d4391524b015f1c256f01becd9d9
João Marcelo <joaojmag@gmail.com>
2024-02-20 17:43:54 -0300

    atualizou

--- Reactjs/aula44_ManipulandoFormularioP2/my-app/src/App.js
+++ Reactjs/aula44_ManipulandoFormularioP2/my-app/src/App.js
@@ -4,42 +4,43 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: 'teste@teste.com',
-      senha: '13456',
-      sexo: 'masculino'
-
+      from: {
+        nome: '',
+        email: '',
+        senha: '',
+        sexo: ''
+      }
     }
-    this.trocaEmail = this.trocaEmail.bind(this);
-    this.trocaSexo = this.trocaSexo.bind(this)
+    this.dadosForm = this.dadosForm.bind(this)
   }
 
-  trocaEmail(e) {
-    let novoValor = e.target.value;
-    this.setState({ email: novoValor })
+  dadosForm(e) {
+    let deform = this.state.from;
+    deform['e.target.name'] = e.target.value;
+    this.setState({ from: deform })
   }
 
-  trocaSexo(e) {
-    let novoSexo = e.target.value;
-    this.setState({ sexo: novoSexo })
-  }
 
   render() {
     return (
       <div>
         <h2>Login</h2>
+        Nome:
+        <input type="text" name="nome" value={this.state.from.nome} onChange={this.dadosForm}></input><br />
         Email:
-        <input type="email" name="email" value={this.state.email} onChange={this.trocaEmail}></input><br />
+        <input type="email" name="email" value={this.state.from.email} onChange={this.dadosForm}></input><br />
         Senha:
-        <input type="password" name="senha" value={this.state.senha} onChange={(e) => this.setState({ senha: e.target.value })}></input> <br />
+        <input type="password" name="senha" value={this.state.from.senha} onChange={(e) => this.setState({ senha: e.target.value })}></input> <br />
         Sexo:
-        <select name='sexo' value={this.state.sexo} onChange={this.trocaSexo}>
+        <select name='sexo' value={this.state.from.sexo} onChange={this.dadosForm}>
           <option value='masculino'>Masculino</option>
           <option value='feminino'>Feminino</option>
         </select>
 
-        <h3>{this.state.email}</h3>
-        <h3>{this.state.senha}</h3>
-        <h3>{this.state.sexo}</h3>
+        <h3>{this.state.from.nome}</h3>
+        <h3>{this.state.from.email}</h3>
+        <h3>{this.state.from.senha}</h3>
+        <h3>{this.state.from.sexo}</h3>
       </div>
     );
   }
