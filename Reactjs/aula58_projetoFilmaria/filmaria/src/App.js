1a2c16c89905bca044286dea4ac3bd975e40fa36
João Marcelo <joaojmag@gmail.com>
2024-02-22 17:39:01 -0300

    add parte do 58

---
+++ Reactjs/aula58_projetoFilmaria/filmaria/src/App.js
@@ -0,0 +1,12 @@
+import RoutesApp from './routes';
+
+
+function App() {
+  return (
+    <div className="app">
+      Oii
+    </div>
+  );
+}
+
+export default App;
---
+++ Reactjs/aula58_projetoFilmaria/filmaria/src/pages/Filme/index.js
@@ -0,0 +1,9 @@
+function Filme() {
+    return (
+        <div>
+            <h1>Bem vindo a Fimes</h1>
+        </div>
+    );
+}
+
+export default Filme;
