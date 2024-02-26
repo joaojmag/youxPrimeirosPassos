a8d3de538f125284fb5f1383e822353b80f49d28
João Marcelo <joaojmag@gmail.com>
2024-02-26 11:59:49 -0300

    add 75 76 77

---
+++ Reactjs/aula75/resumo_76.js
@@ -0,0 +1,122 @@
+//Não funciona 
+
+import { useState } from 'react'
+import { db } from './firebaseConnection';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore'
+
+import './app.css';
+
+function App() {
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+
+  const [posts, setPosts] = useState([]);
+
+
+  async function handleAdd(){
+    // await setDoc(doc(db, "posts", "12345"), {
+    //   titulo: titulo,
+    //   autor: autor,
+    // })
+    // .then(() => {
+    //   console.log("DADOS REGISTRADO NO BANCO!")
+    // })
+    // .catch((error) => {
+    //   console.log("GEROU ERRO" + error)
+    // }) 
+
+
+    await addDoc(collection(db, "posts"), {
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(() => {
+      console.log("CADASTRADO COM SUCESSO")
+      setAutor('');
+      setTitulo('')
+    })
+    .catch((error) => {
+      console.log("ERRO " + error)
+    })
+
+
+  }
+
+
+  async function buscarPost(){
+    // const postRef = doc(db, "posts", "vFvZAyFqebXFsFv0X89l")
+    // await getDoc(postRef)
+    // .then((snapshot) => {
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
+
+    // })
+    // .catch(()=>{
+    //   console.log("ERRO AO BUSCAR")
+    // })
+
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
+    .then((snapshot) => {
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+
+      setPosts(lista);
+
+    })
+    .catch((error) => {
+      console.log("DEU ALGUM ERRO AO BUSCAR")
+    })
+
+
+  }
+
+  return (
+    <div>
+      <h1>ReactJS + Firebase :)</h1>
+
+    <div className="container">
+      <label>Titulo:</label>
+      <textarea 
+        type="text"
+        placeholder='Digite o titulo'
+        value={titulo}
+        onChange={ (e) => setTitulo(e.target.value) }
+      />
+
+      <label>Autor:</label>
+      <input 
+        type="text" 
+        placeholder="Autor do post"
+        value={autor}
+        onChange={(e) => setAutor(e.target.value) }
+      />
+
+      <button onClick={handleAdd}>Cadastrar</button>
+      <button onClick={buscarPost}>Buscar post</button>
+
+      <ul>
+        {posts.map( (post) => {
+          return(
+            <li key={post.id}>
+              <span>Titulo: {post.titulo} </span> <br/>
+              <span>Autor: {post.autor}</span> <br/>  <br/>
+            </li>
+          )
+        })}
+      </ul>
+
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
