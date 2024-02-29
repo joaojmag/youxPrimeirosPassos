b88e255a2e71f38c90c1e7509148e11b2a90aa07
João Marcelo <joaojmag@gmail.com>
2024-02-29 18:19:24 -0300

    add profile

--- Reactjs/aula96/meu-proj/src/pages/Profile/index.js
+++ Reactjs/aula96/meu-proj/src/pages/Profile/index.js
@@ -1,8 +1,14 @@
+import { useContext, useState } from 'react';
 import Header from '../../components/Header'
 import Title from '../../components/Title';
-import { FiSettings } from 'react-icons/fi';
+import { FiSettings, FiUpload } from 'react-icons/fi';
+import avatar from '../../assets/avatar.png'
+import { AuthContext } from '../../contexts/auth'
+import './profile.css'
 
 export default function Profile() {
+    const { user } = useContext(AuthContext)
+    // const [avatarUrl, setAvatarUrl] = useState(avatar)
     return (
         <div>
             <Header />
@@ -10,9 +16,25 @@ export default function Profile() {
                 <Title name='Minha conta'>
                     <FiSettings size={25} />
                 </Title>
+
+                <div className='container'>
+
+                    <form className='form-profile'>
+                        <label className='lable-avatar'>
+                            <samp>
+                                <FiUpload color='#fff' size={25} />
+                            </samp>
+                            <input type='file' accept='image/*' /> <br />
+                            {<img src={avatar} />}
+
+                        </label>
+
+
+
+                    </form>
+                </div>
             </div>
 
-            <h1>Pagina profile</h1>
         </div>
     );
 }
