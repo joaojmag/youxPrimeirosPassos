f77e1911f921b694e57c0e7999a0ddcea482663a
joaojmag <joaojmag@gmail.com>
2024-03-20 00:01:24 -0300

    add texto na pagina 2

--- florestaSustentavel/floresta-sustentavel/src/pages/PainelFlorestal/index.js
+++ florestaSustentavel/floresta-sustentavel/src/pages/PainelFlorestal/index.js
@@ -31,12 +31,53 @@ function PainelFlorestal() {
                     <img src={mapBrasil} />
                 </div>
                 <div className='painel-florestal'>
-                    <button className='btn-Painel-Florestal' /* onClick={voltarPaginaInicial} */> <img src={voltar} /> Painel Florestal</button>
+                    <button className='btn-Painel-Florestal' onClick={voltarPaginaInicial}> <img src={voltar} />
+                        <h1> Painel Florestal</h1>
+                    </button>
+
                     <div className='dvi-florestas-producao'>
                         <button><img src={VectorBR} /> Florestas do Brasil</button>
                         <button> <img src={containerBox} />Produção Florestal</button>
                     </div>
+
                     <button className='btn-Filtrar-Parametros'> <img src={slidershorizontal} />Filtrar Parâmetros</button>
+
+                    <article>
+                        <section>
+                            <h1>Grandeza total das Florestas</h1>
+                            <hr />
+                            <div>
+                                <div><h2>494.458.329 ha</h2> <h3>Área total de Florestas em 2020</h3></div>
+                                <div><h2>9.616.157 ha</h2> <h3>Área total de Florestas Plantadas em 2020</h3></div>
+                                <div><h2>484.842.172 ha</h2> <h3>Área total de Florestas Naturais em 2020</h3></div>
+                            </div>
+                        </section>
+                        <section>
+                            <h1>Florestas nos Biomas Brasileiros</h1>
+                            <hr />
+                            <div>up</div>
+                        </section>
+                        <section>
+                            <h1>Impactos Socio-econômicos</h1>
+                            <hr />
+                            <div>up</div>
+                        </section>
+                        <section>
+                            <h1>Florestas Plantadas</h1>
+                            <hr />
+                            <div>up</div>
+                        </section>
+                        <section>
+                            <h1>Florestas Naturais</h1>
+                            <hr />
+                            <div>up</div>
+                        </section>
+                        <section>
+                            <h1>Áreas Legalmente Protegidas</h1>
+                            <hr />
+                            <div>up</div>
+                        </section>
+                    </article>
                 </div>
                 <Footer />
             </div>
