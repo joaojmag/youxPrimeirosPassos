ec13b3343e072766d31e0ad1e3ee9ff0d28390c7
João Marcelo <joaojmag@gmail.com>
2024-04-03 20:59:47 -0300

    mexendo no head

--- florestaSustentavel/floresta-sustentavel/src/components/Header/index.js
+++ florestaSustentavel/floresta-sustentavel/src/components/Header/index.js
@@ -1,33 +1,69 @@
 // import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import './header.css'
+import { useState } from 'react';
 
 export default function Header() {
 
-    function mudarClasse(e) {
-        console.log(e.target.className);
-        e.target.className = 'sidebar-top-font-primario'
-        // e.target.classList.replace('sidebar-top-font-secundario', 'sidebar-top-font-primario');
-    }
+    const listaMenus = [
+        {
+            label: 'Início',
+            path: '/'
+        },
+        {
+            label: 'Painel Florestal',
+            path: '/painel-florestal/florestas-brasil'
+        },
+        {
+            label: 'Ações e Resultados',
+            path: '/acoes-resultados'
+        },
+        {
+            label: 'Painel Geo',
+            path: '/'
+        },
+        {
+            label: 'Rede',
+            path: '/rede'
+        },
+
+    ]
+
+    const [menuSelecionado, setMenuSelecionado] = useState('')
 
     return (
         <div className='sidebar-primaria'>
             <div className='sidebar-top'>
-                <Link to='/' className='sidebar-top-font-secundario' onClick={mudarClasse}>
+
+                {listaMenus.map((e) => {
+                    return (
+                        <Link to={e.path} onClick={() => setMenuSelecionado('inicio')}
+                            style={menuSelecionado == 'inicio' ? { color: '#ABBF3B', fontWeight: 'bold' } : {}}>
+                            {e.label}
+                        </Link>
+                    )
+                })}
+
+                {/* <Link to='/' className='sidebar-top-font-secundario' onClick={() => setMenuSelecionado('inicio')}
+                    style={menuSelecionado == 'inicio' ? { color: '#ABBF3B', fontWeight: 'bold' } : {}}
+                >
                     Início
                 </Link>
-                <Link to='/painel-florestal/florestas-brasil' className='sidebar-top-font-secundario' onClick={mudarClasse}>
+                <Link to='/painel-florestal/florestas-brasil' className='sidebar-top-font-secundario'
+                    onClick={() => setMenuSelecionado('painelFlorestal')}>
                     Painel Florestal
                 </Link>
-                <Link to='/acoes-resultados' className='sidebar-top-font-secundario' onClick={mudarClasse}>
+                <Link to='/acoes-resultados' className='sidebar-top-font-secundario' onClick={() => setMenuSelecionado('painelFlorestal')}>
                     Ações e Resultados
                 </Link>
-                <Link to='/' className='sidebar-top-font-secundario' onClick={mudarClasse}>
+                <Link to='/' className='sidebar-top-font-secundario' onClick={() => setMenuSelecionado('painelFlorestal')}>
                     Painel Geo
                 </Link>
-                <Link to='/rede' className='sidebar-top-font-secundario' onClick={mudarClasse}>
+                <Link to='/rede' className='sidebar-top-font-secundario' onClick={() => setMenuSelecionado('painelFlorestal')}>
                     Rede
-                </Link>
+                </Link> */}
+
+
             </div>
             <div className='sidebar-bottom'></div>
         </div>
