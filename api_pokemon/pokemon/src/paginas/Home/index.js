20fd4e343140ba0385930b6555ab1f9af5cce2b4
joaojmag <joaojmag@gmail.com>
2024-04-19 01:15:02 -0300

    ideias para fazer o filtro

--- api_pokemon/pokemon/src/paginas/Home/index.js
+++ api_pokemon/pokemon/src/paginas/Home/index.js
@@ -8,15 +8,23 @@ import './home.css'
 export default function Home() {
     const [arrPokmons, setArrPokmons] = useState([]);
     const [pagina, setPagina] = useState(1);
+    const [arrFilter, setArrFilter] = useState([]);
 
     function quantidadeInicial() {
-        let res = (pagina - 1) * 9 + 1
-        return res
+        return (pagina - 1) * 9 + 1
     }
 
     function changePage(nun) {
         setPagina(nun)
     }
+
+
+    function funcFind() {
+        /* const busca = arrPokmons.filter((e) => e.name.includes('sau'));
+        console.log('busca :>> ', busca); */
+        setArrFilter(arrPokmons.filter((e) => e.name.includes('sau')))
+    }
+
 
     useEffect(() => {
         let num = quantidadeInicial()
@@ -31,14 +39,16 @@ export default function Home() {
             }
         }
 
+        funcFind()
         loadingPokemon();
     }, [pagina]);
 
     return (
         <>
             <div className='class-div-home' >
+                {/* tentar fazer um if */}
 
-                {arrPokmons.map((e) => {
+                {(arrFilter ? arrFilter : arrPokmons).map((e) => {
                     return (
                         <>
                             <Card
@@ -47,6 +57,7 @@ export default function Home() {
                                 urlImage={e.imageUrl}
                                 color={e.color}
                             />
+                            {console.log(arrFilter)}
                         </>
                     );
                 })}
