4dd2df2f4fb7f07209a072fe24435ef2ed06bd5c
João Marcelo <joaojmag@gmail.com>
2024-05-06 17:50:06 -0300

    add input nextLine

---
+++ Java/src/aula1_23/entrada_de_dados2.java
@@ -0,0 +1,19 @@
+package aula1_23;
+
+import java.util.Scanner;
+
+public class entrada_de_dados2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a, b, c;
+
+        a = sc.nextLine();
+        b = sc.nextLine();
+        c = sc.nextLine();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
