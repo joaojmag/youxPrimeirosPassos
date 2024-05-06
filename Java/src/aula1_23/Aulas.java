3f8be7cffceeaa12328e9fe313849f802e94125f
João Marcelo <joaojmag@gmail.com>
2024-05-06 14:43:36 -0300

    fazer do code

---
+++ Java/src/aula1_23/Aulas.java
@@ -0,0 +1,17 @@
+package aula1_23;
+
+public class Aulas {
+    public static void main(String[] args) {
+        int y = 32;
+        double x = 10.356987;
+        String nome = "Maria";
+
+        System.out.println("Olá -> " + y);
+        System.out.println(x);
+        System.out.printf("%.4f%n", x);
+        System.out.printf("%.4f%n %d%n", x, y);
+        System.out.printf("%s tem %d anos e uma dívida de %.4f%n", nome, y, x);
+        System.out.print("João");
+
+    }
+}
---
+++ Java/src/aula1_23/Exercicio_23.java
@@ -0,0 +1,30 @@
+package aula1_23;
+
+import java.util.Locale;
+
+public class Exercicio_23 {
+    public static void main(String[] args) {
+        String produdo1 = "Computador";
+        String produto2 = "Office desk";
+
+        int age = 30;
+        int cod = 507845;
+        char genero = 'F';
+
+        double preco1 = 2158.0;
+        double preco2 = 618.0;
+        double measure = 25.78787578754521;
+
+        System.out.println("Produtos");
+        System.out.printf("%s R$ %f%n", produdo1, preco1);
+        System.out.printf("%s R$ %f%n%n", produto2, preco2);
+
+        System.out.printf("Record: %d years old, code %d and genero %c%n", age, cod, genero);
+
+        System.out.printf("Measus with eight decimal places: %f%n", measure);
+        System.out.printf("Measus with eight decimal places: %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("Measus with eight decimal places: %f%n", measure);
+        System.out.print("testes");
+    }
+}
