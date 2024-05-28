67f3344a66efe135de315ec42f2392caf9084cea
João Marcelo <joaojmag@gmail.com>
2024-05-28 14:44:33 -0300

    add pilha stack trace

---
+++ Java/src/aula167_178/try_catch/Program.java
@@ -0,0 +1,23 @@
+package aula167_178.try_catch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Posição invalida");
+        } catch (InputMismatchException e) {
+            System.out.println("Valor invalido");
+        }
+
+        System.out.println("Fim do programa!");
+    }
+}
