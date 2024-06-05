3bec8d6c64c8ec83d972d2fa9a6a7ac32ee3516a
João Marcelo <joaojmag@gmail.com>
2024-06-05 10:29:21 -0300

    add solução do prob 1 (não tão boa)

---
+++ Java/src/aula223_236_Interfaces/introduzindo_assunto_prob1/model/services/BrazilTaxService.java
@@ -0,0 +1,12 @@
+package aula223_236_Interfaces.introduzindo_assunto_prob1.model.services;
+
+public class BrazilTaxService {
+
+    public double tax(double amount) {
+        if (amount <= 100.0)
+            return amount * 0.2;
+        else
+            return amount * 0.15;
+    }
+
+}
