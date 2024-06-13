f7f4bd99fe474aeb086a5045b2f275cade12bb74
João Marcelo <joaojmag@gmail.com>
2024-06-13 10:17:19 -0300

    Demo1.java Product.java  Program.java

---
+++ Java/src/aula237_250_GenericsSetMap/Set/SetTestaIgualdade/Program.java
@@ -0,0 +1,15 @@
+package aula237_250_GenericsSetMap.Set.SetTestaIgualdade;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Program {
+    public static void main(String[] args) {
+        Set<Product> set = new HashSet<>();
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+        Product prod = new Product("Notebook", 1200.0);
+        System.out.println(set.contains(prod));
+    }
+}
