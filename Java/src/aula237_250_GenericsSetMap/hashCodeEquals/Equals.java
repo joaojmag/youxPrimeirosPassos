1b638020debb7ba066d604379fbc519647305133
João Marcelo <joaojmag@gmail.com>
2024-06-13 08:52:27 -0300

    Equals HashCode

---
+++ Java/src/aula237_250_GenericsSetMap/hashCodeEquals/Equals.java
@@ -0,0 +1,11 @@
+package aula237_250_GenericsSetMap.hashCodeEquals;
+
+public class Equals {
+    public static void main(String[] args) {
+
+        String a = "Maria";
+        String b = "Alex";
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+    }
+}
---
+++ Java/src/aula237_250_GenericsSetMap/hashCodeEquals/HashCode.java
@@ -0,0 +1,9 @@
+package aula237_250_GenericsSetMap.hashCodeEquals;
+
+public class HashCode {
+    public static void main(String[] args) {
+        String a = "Maria";
+        String b = "Alex";
+        System.out.println(a.equals(b));
+    }
+}
