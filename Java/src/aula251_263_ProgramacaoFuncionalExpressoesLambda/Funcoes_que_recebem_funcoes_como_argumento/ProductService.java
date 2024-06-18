ad9d132bd76a1e7fac9aa16b3bbc9d1fc14d1573
João Marcelo <joaojmag@gmail.com>
2024-06-18 17:17:27 -0300

    atualizando ProductService.java Program.java

--- Java/src/aula251_263_ProgramacaoFuncionalExpressoesLambda/Funcoes_que_recebem_funcoes_como_argumento/ProductService.java
+++ Java/src/aula251_263_ProgramacaoFuncionalExpressoesLambda/Funcoes_que_recebem_funcoes_como_argumento/ProductService.java
@@ -1,12 +1,13 @@
 package aula251_263_ProgramacaoFuncionalExpressoesLambda.Funcoes_que_recebem_funcoes_como_argumento;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ProductService {
-    public double filteredSum(List<Product> list) {
+    public double filteredSum(List<Product> list, Predicate<Product> criteria) {
         double sum = 0.0;
         for (Product p : list) {
-            if (p.getName().charAt(0) == 'T') {
+            if (criteria.test(p)) {
                 sum += p.getPrice();
             }
         }
