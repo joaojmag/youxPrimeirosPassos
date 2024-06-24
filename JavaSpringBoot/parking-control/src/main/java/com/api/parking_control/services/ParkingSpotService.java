6c5dc1c56d36ea9696ae097010fb846b7c549de2
João Marcelo <joaojmag@gmail.com>
2024-06-24 11:06:57 -0300

    add arquivos ParkingSpotController

---
+++ JavaSpringBoot/parking-control/src/main/java/com/api/parking_control/services/ParkingSpotService.java
@@ -0,0 +1,14 @@
+package com.api.parking_control.services;
+
+import com.api.parking_control.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+  final  ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+}
