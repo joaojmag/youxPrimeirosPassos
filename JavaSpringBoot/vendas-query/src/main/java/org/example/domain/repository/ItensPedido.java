91afa926120fa08750466dfb5198e0c56fb9c89b
João Marcelo <joaojmag@gmail.com>
2024-06-28 15:57:30 -0300

    criando os repositorios das entidades

---
+++ JavaSpringBoot/vendas-query/src/main/java/org/example/domain/repository/ItensPedido.java
@@ -0,0 +1,6 @@
+package org.example.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItensPedido extends JpaRepository<ItensPedido, Integer> {
+}
