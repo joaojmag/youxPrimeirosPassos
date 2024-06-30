cbaaf2c8b92b7221039cb76ed543fcb48cda3902
João Marcelo <joaojmag@gmail.com>
2024-06-30 14:25:50 -0300

    começando nova seção 4

--- JavaSpringBoot/vendas-query/src/main/java/org/example/domain/repository/ItensPedido.java
+++
@@ -1,6 +0,0 @@
-package org.example.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItensPedido extends JpaRepository<ItensPedido, Integer> {
-}
