c579a039eaf6cf07ab0e691975d6691aa34bb626
João Marcelo <joaojmag@gmail.com>
2024-07-12 11:58:10 -0300

    primeiro commit da casa de apostas

---
+++ Casa_De_Aposta/casa-de-apostas/src/main/java/com/jmag/casa_de_apostas/CasaDeApostasApplication.java
@@ -0,0 +1,13 @@
+package com.jmag.casa_de_apostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDeApostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDeApostasApplication.class, args);
+	}
+
+}
