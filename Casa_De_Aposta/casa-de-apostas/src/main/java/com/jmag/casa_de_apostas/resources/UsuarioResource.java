2fae02f2db73c43cf09152fdf151fa7994730990
João Marcelo <joaojmag@gmail.com>
2024-07-16 10:42:36 -0300

    casa-de-apostas senha cadastrada

--- Casa_De_Aposta/casa-de-apostas/src/main/java/com/jmag/casa_de_apostas/resources/UsuarioResource.java
+++ Casa_De_Aposta/casa-de-apostas/src/main/java/com/jmag/casa_de_apostas/resources/UsuarioResource.java
@@ -2,6 +2,7 @@ package com.jmag.casa_de_apostas.resources;
 
 import com.jmag.casa_de_apostas.entities.Usuario;
 import com.jmag.casa_de_apostas.repositories.UsuarioRepository;
+import com.jmag.casa_de_apostas.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,30 +17,33 @@ public class UsuarioResource {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private UsuarioService service;
+
     @GetMapping("/{id}")
     public Usuario getUsuarioById(@PathVariable Integer id) {
-        return usuarioRepository
+        return service
                 .findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping
     public List<Usuario> getUsuarios() {
-        return usuarioRepository.findAll();
+        return service.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Usuario save(@RequestBody Usuario usuario) {
-        return usuarioRepository.save(usuario);
+        return service.save(usuario);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Integer id) {
-        usuarioRepository.findById(id)
+        service.findById(id)
                 .map(e -> {
-                    usuarioRepository.delete(e);
+                    service.delete(e.getId());
                     return e;
                 })
                 .orElseThrow(() ->
@@ -49,12 +53,11 @@ public class UsuarioResource {
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@PathVariable Integer id, @RequestBody Usuario usuario) {
-        usuarioRepository.findById(id).map(e -> {
-            usuario.setId(e.getId());
-            usuarioRepository.save(usuario);
-            return e;
-        }).orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND));
+        try {
+            service.update(id, usuario);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- Casa_De_Aposta/casa-de-apostas/src/main/java/com/jmag/casa_de_apostas/services/UsuarioService.java
+++ Casa_De_Aposta/casa-de-apostas/src/main/java/com/jmag/casa_de_apostas/services/UsuarioService.java
@@ -1,4 +1,72 @@
 package com.jmag.casa_de_apostas.services;
 
+import com.jmag.casa_de_apostas.entities.Usuario;
+import com.jmag.casa_de_apostas.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    public Optional<Usuario> findById(Integer id) {
+        return repository.findById(id);
+    }
+
+    public List<Usuario> findAll() {
+        return repository.findAll();
+    }
+
+    public Usuario save(Usuario usuario) {
+
+        if (verificarSenha(usuario)) {
+            return repository.save(usuario);
+        } else
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    public void delete(Integer id) {
+        repository.findById(id)
+                .map(e -> {
+                    repository.delete(e);
+                    return e;
+                });
+    }
+
+    public void update(Integer id, Usuario usuario) {
+        repository
+                .findById(id)
+                .map(e -> {
+                    usuario.setId(e.getId());
+                    repository.save(usuario);
+                    return e;
+                }).orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    private boolean verificarSenha(Usuario usuario) {
+
+        if (usuario.getSenha().length() >= 8) {
+            // Expressão regular para verificar se contém pelo menos uma letra maiúscula, um número e um caractere especial
+            String chaveParaVerificacao = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$";
+            String senha = usuario.getSenha();
+
+            Pattern pattern = Pattern.compile(chaveParaVerificacao);
+            Matcher matcher = pattern.matcher(senha);
+
+            return matcher.matches();
+        } else
+            return false;
+    }
+
 }
+
