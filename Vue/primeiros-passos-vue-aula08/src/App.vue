d7a5538d23183c34407a904a96450a54e00be3b2
João Marcelo <joaojmag@gmail.com>
2024-07-30 13:53:23 -0300

    primeiros-passos-vue-aula09

---
+++ Vue/primeiros-passos-vue-aula08/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+
+  <div>
+    One-way <br>
+    Tow-way<br>
+    v-mode -> formulários
+
+    <div>
+      <label>Nome</label><br />
+      <input v-model="name" type="text"><br />
+      {{ name }}
+    </div>
+
+    <div>
+      <select v-model="sport">
+        <option value="">Escolha</option>
+        <option value="futebol">futebol</option>
+        <option value="tenes">tenes</option>
+        <option value="corrida">corrida</option>
+      </select>
+      {{ sport }}
+    </div>
+
+    <label>Newsletter</label><br />
+    <input v-model="newsletter" type="radio" value="Sim">Sim
+    <input v-model="newsletter" type="radio" value="Não">Não
+    {{ newsletter }}
+
+    <label>Contrato</label><br />
+    <input v-model="contrato" type="checkbox">Aceita nosso termos....
+    {{ contrato }}
+
+    <label>Cores</label><br />
+    <input v-model="cores" type="checkbox" value="Amarelo">Amarelo
+    <input v-model="cores" type="checkbox" value="Azul">Azul
+    {{ cores }}
+
+
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: 'joão',
+      sport: '',
+      newsletter: '',
+      contrato: false,
+      cores:[]
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ Vue/primeiros-passos-vue-aula10/src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <button v-on:click="capitura()">
+      Enviar
+    </button>
+
+    <button @click="outraforma">
+      Outra forma
+    </button>
+
+    <button @mouseover="mouseOver()" @mouseout="mouseOut()">
+      mouseOver
+    </button>
+
+    <button @mouseout="mouseOut()">
+      mouseOver
+    </button>
+    <br />
+
+    <form action="https://google.com" v-on:submit.prevent="onSubmit">
+      <input type="text" v-on:keyup.enter="onKyuo">
+
+      <button type="submit">Enviar</button>
+
+    </form>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    capitura() {
+      console.log('click :>> ');
+    },
+    outraforma($evt) {
+      console.log('oura forma :>> ', $evt);
+    },
+    mouseOver() {
+      console.log('mouseOver');
+    },
+    mouseOut() {
+      console.log('mouseOut');
+    },
+    onSubmit() {
+      console.log("submite");
+    },
+    onKyuo(evento) {
+      console.log('onKyuo', evento);
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
